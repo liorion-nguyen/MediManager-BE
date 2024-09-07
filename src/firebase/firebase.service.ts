929b2fc7baf293e8ba8d81fd3c84d527c808ebac
Nguyễn Quốc Chung <quocchung.starack@gmail.com>
2024-09-07 23:53:09 +0700

    fix: firebase

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
-import * as file from "./firebase-adminsdk.json"
 
 @Injectable()
 export class FirebaseService {
 
-  private static isInitialized = false; // Static variable to track initialization status
+  private static isInitialized = false;
   private readonly storage: admin.storage.Storage;
 
   constructor() {
     if (!FirebaseService.isInitialized) {
-      const serviceAccount = require('./firebase-adminsdk.json');
+      const serviceAccount: any = {
+        type: process.env.FIREBASE_TYPE,
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_CLIENT_ID,
+        auth_uri: process.env.FIREBASE_AUTH_URI,
+        token_uri: process.env.FIREBASE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+        universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+      }
       admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
-        storageBucket: 'medimanager-liorion.appspot.com',
+        storageBucket: process.env.URL_Bucket_Firebase,
       });
       FirebaseService.isInitialized = true; // Mark Firebase as initialized
     }
